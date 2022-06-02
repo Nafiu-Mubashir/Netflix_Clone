3d023d6556047278f925cca31402e1c4d1bbe843
Nafiu Mubashir <mubashirnafiu@gmail.com>
2022-06-02 08:19:49 +0100

    medial query and hover effect

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,53 +2,60 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import "../components/style.css"
 function Cards() {
-    // const url = "https://imdb-api.com/API/AdvancedSearch/k_s7l8kj1r/?genres=action,adventure";
-    const url = "https://api.themoviedb.org/3/discover/movie?api_key=ee3db26c13a95de355febc98d8b68dbf";
-    const [images, setimages] = useState([]);
-    const axios = require("axios");
-    const [error, seterror] = useState("");
-    const [isLoading, setisLoading] = useState(true)
-    useEffect(() => {
-        makeRequest()
-    }, [])
-    
-    const makeRequest = () => {
-        axios.get(url).then((res) => {
-            if (res.status == 200) {
-                console.log(res.data);
-                setimages(res.data.results)
-            }
-            else{
-                console.log("An error occur");
-            }
-        }).catch((err) => {
-            seterror(err);
-        })
-        // setisLoading(false)
-    }
+  // const url = "https://imdb-api.com/API/AdvancedSearch/k_s7l8kj1r/?genres=action,adventure";
+  const url = "https://api.themoviedb.org/3/discover/movie?api_key=ee3db26c13a95de355febc98d8b68dbf";
+  const [images, setimages] = useState([]);
+  const axios = require("axios");
+  const [error, seterror] = useState("");
+  const [isLoading, setisLoading] = useState(true)
+  useEffect(() => {
+    makeRequest()
+  }, [])
+
+  const makeRequest = () => {
+    axios.get(url).then((res) => {
+      if (res.status == 200) {
+        console.log(res.data);
+        setimages(res.data.results)
+      }
+      else {
+        console.log("An error occur");
+      }
+    }).catch((err) => {
+      seterror(err);
+    })
+    // setisLoading(false)
+  }
   return (
     <>
-    <div className="container-fluid">
-        <div className="row p-2"> 
-          {images.map((user,index)=>(
-             <div className="card bg-dark p-2">
-             {/* <img src={user.image} className="card-img-top" alt="..."/> */}
-             <img src={`https://image.tmdb.org/t/p/w185` + user.backdrop_path} className="card-img-top" alt="..."/>
-   
-             <div className="card-body">
-             <p className="text-white">{user.original_title}</p>
-               {/* <div>
+      <div className="container-fluid bg-dark">
+        <div className="row p-2">
+          {images.map((user, index) => (
+            <div className="card bg-dark shadow p-2">
+              {/* <img src={user.image} className="card-img-top" alt="..."/> */}
+              <img src={`https://image.tmdb.org/t/p/w185` + user.backdrop_path} className="card-img-top" alt="..." />
+
+              <div className="card-body">
+                <p className="text-white">{user.original_title}</p>
+               <div>
+               <i class="fa-solid fa-circle-play text-white"></i>
+                <i class="fa-solid fa-circle-plus text-white mx-2"></i>
+                <i class="fa-solid fa-thumbs-up text-white"></i>
+                {/* <i class="fa-solid fa-circle-chevron-down text-white mx-auto"></i> */}
+               </div>
+
+                {/* <div>
                     <p className="text-white">{user.title}</p>
                     <p className="text-white mb-3">{user.genres}</p>
                </div> */}
-               
-             </div>
-           </div>
-   
-            
+
+              </div>
+            </div>
+
+
           ))}
         </div>
-    </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,16 +12,16 @@ function Navbar() {
                     className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navmenu">
                     <ul className="navbar-nav ms-5">
-                        <li className='nav-item'><Link to="/" className='nav-link'>Home</Link></li>
-                        <li className='nav-item'><Link to="/about" className='nav-link'>TV Shows</Link></li>
-                        <li className='nav-item'><Link to="/products" className='nav-link'>Movies</Link></li>
-                        <li className='nav-item'><Link to="/products" className='nav-link'>New & Popular</Link></li>
-                        <li className='nav-item'><Link to="/products" className='nav-link'>My List</Link></li>
+                        <li className='nav-item'><Link to="/" className='nav-link text-white'>Home</Link></li>
+                        <li className='nav-item'><Link to="/about" className='nav-link text-white'>TV Shows</Link></li>
+                        <li className='nav-item'><Link to="/products" className='nav-link text-white'>Movies</Link></li>
+                        <li className='nav-item'><Link to="/products" className='nav-link text-white'>New & Popular</Link></li>
+                        <li className='nav-item'><Link to="/products" className='nav-link text-white'>My List</Link></li>
                     </ul>
                     <div className='navbar-nav ms-auto'>
-                    <i className="fa-solid fa-magnifying-glass text-white mt-2"></i>
-                    <Link to="/products" className='nav-link'>Kids</Link>
-                    <i class="fa-solid fa-bell text-white mt-2"></i>
+                    <i className="fa-solid fa-magnifying-glass text-white"></i>
+                    <Link to="/products" className='nav-link text-white'>Kids</Link>
+                    <i class="fa-solid fa-bell text-white"></i>
                     {/* <FontAwesomeIcon icon="fa-solid fa-bell" /> */}
                     </div>
                 </div>
